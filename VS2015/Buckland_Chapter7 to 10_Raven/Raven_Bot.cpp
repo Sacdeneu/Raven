9f05bbf5e7f80aa03c574e46af5e9fda915affa6
Sulliman Abdul <sulliman.abdul@gmail.com>
2018-12-02 19:46:41 -0500

    ajout réseau neuronal
    Ajout du réseau neuronal, mais on a rien configuré, il faut arriver a mettre en place les critères d'entrainement cités dans la question, et  voir comment on peut structurer le fichier de données.
    Si t'y arrives pas autant faire les autres question pour gratter le plus de points

--- VS2015/Buckland_Chapter7 to 10_Raven/Raven_Bot.cpp
+++ VS2015/Buckland_Chapter7 to 10_Raven/Raven_Bot.cpp
@@ -11,7 +11,7 @@
 #include "time/Regulator.h"
 #include "Raven_WeaponSystem.h"
 #include "Raven_SensoryMemory.h"
-
+#include "floatfann.h"
 #include "Messaging/Telegram.h"
 #include "Raven_Messages.h"
 #include "Messaging/MessageDispatcher.h"
@@ -81,6 +81,66 @@ Raven_Bot::Raven_Bot(Raven_Game* world,Vector2D pos):
                                         script->GetDouble("Bot_AimPersistance"));
 
   m_pSensoryMem = new Raven_SensoryMemory(this, script->GetDouble("Bot_MemorySpan"));
+
+  fann_type *calc_out;
+  const unsigned int num_input = 4;
+  const unsigned int num_output = 1;
+  const unsigned int num_layers = 3;
+  const unsigned int num_neurons_hidden = 3;
+  const float desired_error = (const float)0;
+  const unsigned int max_epochs = 1000;
+  const unsigned int epochs_between_reports = 10;
+  struct fann *ann;
+  struct fann_train_data *data;
+
+  unsigned int i = 0;
+  unsigned int decimal_point;
+
+  printf("Creating network.\n");
+  ann = fann_create_standard(num_layers, num_input, num_neurons_hidden, num_output);
+
+  data = fann_read_train_from_file("xor.data");
+
+  fann_set_activation_steepness_hidden(ann, 1);
+  fann_set_activation_steepness_output(ann, 1);
+
+  fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
+  fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
+
+  fann_set_train_stop_function(ann, FANN_STOPFUNC_BIT);
+  fann_set_bit_fail_limit(ann, 0.01f);
+
+  fann_set_training_algorithm(ann, FANN_TRAIN_RPROP);
+
+  fann_init_weights(ann, data);
+
+  printf("Training network.\n");
+  fann_train_on_data(ann, data, max_epochs, epochs_between_reports, desired_error);
+
+  printf("Testing network. %f\n", fann_test_data(ann, data));
+
+  for (i = 0; i < fann_length_train_data(data); i++)
+  {
+	  calc_out = fann_run(ann, data->input[i]);
+	  printf("XOR test (%f,%f) -> %f, should be %f, difference=%f\n",
+		  data->input[i][0], data->input[i][1], calc_out[0], data->output[i][0],
+		  fann_abs(calc_out[0] - data->output[i][0]));
+  }
+
+  printf("Saving network.\n");
+
+  fann_save(ann, "xor_float.net");
+
+  decimal_point = fann_save_to_fixed(ann, "xor_fixed.net");
+  fann_save_train_to_fixed(data, "xor_fixed.data", decimal_point);
+
+  printf("Cleaning up.\n");
+  fann_destroy_train(data);
+  fann_destroy(ann);
+
+
+
+
 }
 
 //-------------------------------- dtor ---------------------------------------
